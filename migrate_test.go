c60c62ec44859858e1a6f6af0767c285eae3fb13
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-23 15:22:44 +0800

    feat: Add migrate testing

--- migrate_test.go
+++ migrate_test.go
@@ -8,12 +8,54 @@ import (
 )
 
 var (
-	dbHost = "mongodb"
-	dhName = "test_migrate"
+	dbHost     = "mongodb"
+	dbName     = "test_migrate"
+	migrations = []*Migration{
+		{
+			ID: "201709201400",
+			Migrate: func(s *mgo.Session) error {
+				return nil
+			},
+			Rollback: func(s *mgo.Session) error {
+				return nil
+			},
+		},
+	}
+
+	initSchema = func(s *mgo.Session) error {
+		return nil
+	}
 )
+
+func dropDB(s *mgo.Session) error {
+	db := s.DB(dbName)
+
+	return db.DropDatabase()
+}
 
 func TestMigrate(t *testing.T) {
 	session, err := mgo.Dial(dbHost)
 	assert.Nil(t, err)
 	defer session.Close()
+
+	m := New(session, dbName, DefaultOptions, migrations)
+	err = m.Migrate()
+	assert.Nil(t, err)
+
+	err = dropDB(session)
+	assert.Nil(t, err)
+}
+
+func TestInitSchema(t *testing.T) {
+	session, err := mgo.Dial(dbHost)
+	assert.Nil(t, err)
+	defer session.Close()
+
+	m := New(session, dbName, DefaultOptions, migrations)
+	m.InitSchema(initSchema)
+	err = m.Migrate()
+	assert.Nil(t, err)
+
+	err = dropDB(session)
+	assert.Nil(t, err)
 }
