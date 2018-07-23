b8b5922e4b1c3475ad8e7ed015a4e81ec1c57fb4
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-23 15:39:32 +0800

    feat: add TestErrNoMigrationDefined

--- migrate_test.go
+++ migrate_test.go
@@ -41,6 +41,8 @@ func TestMigrate(t *testing.T) {
 	m := New(session, dbName, DefaultOptions, migrations)
 	err = m.Migrate()
 	assert.Nil(t, err)
+	err = m.RollbackLast()
+	assert.Nil(t, err)
 
 	err = dropDB(session)
 	assert.Nil(t, err)
@@ -55,6 +57,47 @@ func TestInitSchema(t *testing.T) {
 	m.InitSchema(initSchema)
 	err = m.Migrate()
 	assert.Nil(t, err)
+
+	err = dropDB(session)
+	assert.Nil(t, err)
+}
+
+func TestMissingID(t *testing.T) {
+	session, err := mgo.Dial(dbHost)
+	assert.Nil(t, err)
+	defer session.Close()
+
+	newMigrations := append(migrations, &Migration{
+		ID: "",
+		Migrate: func(s *mgo.Session) error {
+			return nil
+		},
+		Rollback: func(s *mgo.Session) error {
+			return nil
+		},
+	})
+
+	m := New(session, dbName, DefaultOptions, newMigrations)
+	err = m.Migrate()
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrMissingID, err)
+
+	err = dropDB(session)
+	assert.Nil(t, err)
+}
+
+func TestErrNoMigrationDefined(t *testing.T) {
+	session, err := mgo.Dial(dbHost)
+	assert.Nil(t, err)
+	defer session.Close()
+
+	// empty migration
+	m := New(session, dbName, DefaultOptions, []*Migration{})
+	err = m.Migrate()
+	assert.Nil(t, err)
+	err = m.RollbackLast()
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrNoMigrationDefined, err)
 
 	err = dropDB(session)
 	assert.Nil(t, err)
