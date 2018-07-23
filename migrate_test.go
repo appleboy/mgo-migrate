9646097252913374fb0937a73df19a915964f9a5
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-23 15:10:25 +0800

    test: initial

--- migrate_test.go
+++ migrate_test.go
@@ -1,1 +1,19 @@
 package migrate
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/mgo.v2"
+)
+
+var (
+	dbHost = "mongodb"
+	dhName = "test_migrate"
+)
+
+func TestMigrate(t *testing.T) {
+	session, err := mgo.Dial(dbHost)
+	assert.Nil(t, err)
+	defer session.Close()
+}
